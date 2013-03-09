0283dfcd7e6031d69cf10b24369d3d249832f40f
David Sanders <dsanders@fusionbox.com>
2013-03-08 17:49:28 -0700

    Add options to restore specific backup or drop tables

--- backupdb/management/commands/restoredb.py
+++ backupdb/management/commands/restoredb.py
@@ -4,6 +4,7 @@ Adapted from http://djangosnippets.org/snippets/823/
 import glob
 import os
 import pipes
+from optparse import make_option
 from subprocess import Popen, PIPE
 
 from django.core.management.base import BaseCommand, CommandError
@@ -19,7 +20,11 @@ def get_latest_file(pattern):
     l = glob.glob(pattern)
     l.sort()
     l.reverse()
-    return l[0] if l else None
+
+    if not l:
+        raise CommandError('No backups found matching "{0}" pattern!'.format(pattern))
+
+    return l[0]
 
 
 def require_backup_exists(func):
@@ -38,12 +43,27 @@ class Command(BaseCommand):
     help = 'Restores each database in settings.DATABASES from latest db backup.'
     can_import_settings = True
 
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--backup-name',
+            help='Backup to restore from e.g. "mybackup" in "mybackup.pgsql.gz".  Defaults to latest timestamped backup name.',
+        ),
+        make_option(
+            '--drop-tables',
+            action='store_true',
+            default=False,
+            help='Drop all tables in databases before restoring them',
+        ),
+    )
 
     def handle(self, *args, **options):
         from django.conf import settings
 
         if not os.path.exists(BACKUP_DIR):
             raise CommandError('Backup dir \'{0}\' does not exist!'.format(BACKUP_DIR))
+
+        backup_name = options.get('backup_name')
+        drop_tables = options.get('drop_tables')
 
         # Loop through databases and restore
         for database_name in settings.DATABASES:
@@ -53,9 +73,10 @@ class Command(BaseCommand):
             if config['ENGINE'] == 'django.db.backends.mysql':
                 restore_cmd = self.do_mysql_restore
 
-                backup_file = get_latest_file('{0}/*.mysql.gz'.format(BACKUP_DIR))
-                if not backup_file:
-                    raise CommandError('No MySQL backups found!')
+                if backup_name:
+                    backup_file = '{0}/{1}.mysql.gz'.format(BACKUP_DIR, backup_name)
+                else:
+                    backup_file = get_latest_file('{0}/*.mysql.gz'.format(BACKUP_DIR))
 
                 restore_kwargs = {
                     'backup_file': backup_file,
@@ -64,15 +85,17 @@ class Command(BaseCommand):
                     'password': config.get('PASSWORD', None),
                     'host': config.get('HOST', None),
                     'port': config.get('PORT', None),
+                    'drop': drop_tables,
                 }
 
             # PostgreSQL command and args
             elif config['ENGINE'] == 'django.db.backends.postgresql_psycopg2':
                 restore_cmd = self.do_postgresql_restore
 
-                backup_file = get_latest_file('{0}/*.pgsql.gz'.format(BACKUP_DIR))
-                if not backup_file:
-                    raise CommandError('No PostgreSQL backups found!')
+                if backup_name:
+                    backup_file = '{0}/{1}.pgsql.gz'.format(BACKUP_DIR, backup_name)
+                else:
+                    backup_file = get_latest_file('{0}/*.pgsql.gz'.format(BACKUP_DIR))
 
                 restore_kwargs = {
                     'backup_file': backup_file,
@@ -81,20 +104,23 @@ class Command(BaseCommand):
                     'password': config.get('PASSWORD', None),
                     'host': config.get('HOST', None),
                     'port': config.get('PORT', None),
+                    'drop': drop_tables,
                 }
 
             # SQLite command and args
             elif config['ENGINE'] == 'django.db.backends.sqlite3':
                 restore_cmd = self.do_sqlite_restore
 
-                backup_file = get_latest_file('{0}/*.sqlite.gz'.format(BACKUP_DIR))
-                if not backup_file:
-                    raise CommandError('No SQLite backups found!')
+                if backup_name:
+                    backup_file = '{0}/{1}.sqlite.gz'.format(BACKUP_DIR, backup_name)
+                else:
+                    backup_file = get_latest_file('{0}/*.sqlite.gz'.format(BACKUP_DIR))
 
                 restore_kwargs = {
                     'backup_file': backup_file,
                     'db_file': config['NAME'],
                 }
+
             # Unsupported
             else:
                 restore_cmd = None
@@ -116,7 +142,7 @@ class Command(BaseCommand):
             print ''
 
     @require_backup_exists
-    def do_mysql_restore(self, backup_file, db, user, password=None, host=None, port=None):
+    def do_mysql_restore(self, backup_file, db, user, password=None, host=None, port=None, drop=False):
         # Build args to restore command
         restore_args = []
         restore_args += ['--user={0}'.format(pipes.quote(user))]
@@ -143,11 +169,13 @@ class Command(BaseCommand):
         )
 
         # Execute
-        self.do_command(drop_cmd, 'clearing', db)
+        if drop:
+            import pdb; pdb.set_trace()
+            self.do_command(drop_cmd, 'clearing', db)
         self.do_command(restore_cmd, 'restoring', db)
 
     @require_backup_exists
-    def do_postgresql_restore(self, backup_file, db, user, password=None, host=None, port=None):
+    def do_postgresql_restore(self, backup_file, db, user, password=None, host=None, port=None, drop=False):
         # Build args to restore command
         restore_args = []
         restore_args += ['--username={0}'.format(pipes.quote(user))]
@@ -177,7 +205,9 @@ class Command(BaseCommand):
         )
 
         # Execute
-        self.do_command(drop_cmd, 'dropping', db)
+        if drop:
+            import pdb; pdb.set_trace()
+            self.do_command(drop_cmd, 'dropping', db)
         self.do_command(restore_cmd, 'restoring', db)
 
     @require_backup_exists
