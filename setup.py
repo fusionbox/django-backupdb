4a96e7d627f8f4e31cc56c0e1a33b928afed2a6f
David Sanders <dsanders@fusionbox.com>
2013-03-12 18:27:02 -0600

    Set version to 0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 
 setup(name='django-backupdb',
-      version='0.3',
+      version='0.4',
       description='Management commands for backing up and restoring databases in Django.',
       long_description=readme(),
       author='David Sanders',
