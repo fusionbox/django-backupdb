ccbedbacdde82be171ac54ad3b06094fc3f9dc2f
David Sanders <dsanders@fusionbox.com>
2013-06-09 00:20:40 -0600

    Take advantage of functionality in streamtools

--- backupdb/management/commands/backupdb.py
+++ backupdb/management/commands/backupdb.py
@@ -6,9 +6,10 @@ from subprocess import Popen, PIPE
 import os
 import shlex
 import time
-import sys
 
 from django.core.management.base import BaseCommand
+
+from backupdb_utils.streamtools import err, section, SectionError, set_verbosity
 
 
 def do_mysql_backup(backup_file, db_config):
@@ -75,8 +76,7 @@ def pipe_commands_to_file(cmds, path, extra_env=None):
         for cmd in cmds
     ]
 
-    sys.stderr.write('Saving output of:\n')
-    sys.stderr.write(' | '.join(cmd_strs) + '\n')
+    err('* Saving output of `{0}`'.format(' | '.join(cmd_strs)), verbosity=2)
 
     # Start processes
     processes = []
@@ -99,7 +99,7 @@ def pipe_commands_to_file(cmds, path, extra_env=None):
         for cmd_str, p in processes:
             p.stdout.close()
             if p.wait() != 0:
-                raise BackupError('Command `{cmd_str}` returned non-zero exit status `{returncode}`'.format(
+                raise BackupError('Command `{cmd_str}` returned non-zero exit status {returncode}'.format(
                     cmd_str=cmd_str,
                     returncode=p.returncode,
                 ))
@@ -160,42 +160,40 @@ class Command(BaseCommand):
         current_time = time.strftime('%F-%s')
         backup_name = options['backup_name'] or current_time
 
+        set_verbosity(int(options['verbosity']))
+
+        # Ensure backup dir present
         if not os.path.exists(BACKUP_DIR):
             os.makedirs(BACKUP_DIR)
 
         # Loop through databases
         for db_name, db_config in settings.DATABASES.items():
-            print '========== Backing up \'{0}\'...'.format(db_name)
+            with section("Backing up '{0}'...".format(db_name)):
+                # Get options for this engine type
+                engine = db_config['ENGINE']
+                engine_options = ENGINE_OPTIONS.get(engine)
+                if not engine_options:
+                    raise SectionError("! Backup for '{0}' engine not implemented".format(engine))
 
-            engine = db_config['ENGINE']
-            engine_options = ENGINE_OPTIONS.get(engine)
-
-            if not engine_options:
-                print 'Backup for {0} engine not implemented.'.format(engine)
-                print '========== ...skipped.\n'
-                continue
-
-            backup_base_name = '{db_name}-{backup_name}.{backup_extension}.gz'.format(
-                db_name=db_name,
-                backup_name=backup_name,
-                backup_extension=engine_options['backup_extension'],
-            )
-            backup_file = os.path.join(BACKUP_DIR, backup_base_name)
-
-            # Find and run backup command
-            backup_func = engine_options['backup_func']
-
-            backup_kwargs = {'backup_file': backup_file, 'db_config': db_config}
-            if backup_func is do_postgresql_backup:
-                backup_kwargs['pg_dump_options'] = options['pg_dump_options']
-
-            try:
-                backup_func(**backup_kwargs)
-                print 'Backup of `{db}` saved in `{backup_file}`.'.format(
-                    db=db_name,
-                    backup_file=backup_file,
+                # Create backup file name
+                backup_base_name = '{db_name}-{backup_name}.{backup_extension}.gz'.format(
+                    db_name=db_name,
+                    backup_name=backup_name,
+                    backup_extension=engine_options['backup_extension'],
                 )
-                print '========== ...done!\n'
-            except BackupError as e:
-                print e.message
-                print '========== ...skipped.\n'
+                backup_file = os.path.join(BACKUP_DIR, backup_base_name)
+
+                # Find and backup command and get kwargs
+                backup_func = engine_options['backup_func']
+                backup_kwargs = {'backup_file': backup_file, 'db_config': db_config}
+                if backup_func is do_postgresql_backup:
+                    backup_kwargs['pg_dump_options'] = options['pg_dump_options']
+
+                # Run backup command
+                try:
+                    backup_func(**backup_kwargs)
+                    err("* Backup of '{db}' saved in '{backup_file}'".format(
+                        db=db_name,
+                        backup_file=backup_file))
+                except BackupError as e:
+                    raise SectionError('! ' + e.message)
