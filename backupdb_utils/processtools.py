5cd8d6f1bb6da4882b318232dad8ae6bde8f916c
David Sanders <dsanders@fusionbox.com>
2013-06-09 13:10:13 -0600

    Update pipe_commands and pipe_commands_to_file

--- backupdb_utils/processtools.py
+++ backupdb_utils/processtools.py
@@ -4,7 +4,7 @@ import os
 from .streamtools import err
 
 
-def pipe_commands(cmds, extra_env=None):
+def pipe_commands(cmds, extra_env=None, show_stderr=False):
     """
     Executes the list of commands piping each one into the next.
     """
@@ -23,28 +23,29 @@ def pipe_commands(cmds, extra_env=None):
 
     err('* Running `{0}`'.format(' | '.join(cmd_strs)), verbosity=2)
 
-    # Start processes
-    num_cmds = len(cmds)
-    processes = []
-    for i, (cmd_str, cmd) in enumerate(zip(cmd_strs, cmds), 1):
-        p_prev = processes[-1][1] if processes else None
+    with open('/dev/null', 'w') as NULL:
+        # Start processes
+        num_cmds = len(cmds)
+        processes = []
+        for i, (cmd_str, cmd) in enumerate(zip(cmd_strs, cmds), 1):
+            p_prev = processes[-1][1] if processes else None
 
-        p_stdout = None if i == num_cmds else PIPE
-        p_stdin = p_prev.stdout if p_prev else None
+            p_stdout = None if i == num_cmds else PIPE
+            p_stdin = p_prev.stdout if p_prev else None
+            p_stderr = None if show_stderr else NULL
 
-        p_curr = Popen(cmd, env=env, stdout=p_stdout, stdin=p_stdin)
+            p_curr = Popen(cmd, env=env, stdout=p_stdout, stdin=p_stdin, stderr=p_stderr)
+            processes.append((cmd_str, p_curr))
 
-        processes.append((cmd_str, p_curr))
-
-    # Close processes
-    for cmd_str, p in processes:
-        if p.stdout:
-            p.stdout.close()
-        if p.wait() != 0:
-            raise CalledProcessError(cmd=cmd_str, returncode=p.returncode)
+        # Close processes
+        for cmd_str, p in processes:
+            if p.stdout:
+                p.stdout.close()
+            if p.wait() != 0:
+                raise CalledProcessError(cmd=cmd_str, returncode=p.returncode)
 
 
-def pipe_commands_to_file(cmds, path, extra_env=None):
+def pipe_commands_to_file(cmds, path, extra_env=None, show_stderr=False):
     """
     Executes the list of commands piping each one into the next and writing
     stdout of the last process into a file at the given path.
@@ -64,26 +65,30 @@ def pipe_commands_to_file(cmds, path, extra_env=None):
 
     err('* Saving output of `{0}`'.format(' | '.join(cmd_strs)), verbosity=2)
 
-    # Start processes
-    processes = []
-    for cmd_str, cmd in zip(cmd_strs, cmds):
-        p_prev = processes[-1][1] if processes else None
-        p_stdin = p_prev.stdout if p_prev else None
-        p_curr = Popen(cmd, env=env, stdout=PIPE, stdin=p_stdin)
-        processes.append((cmd_str, p_curr))
+    with open('/dev/null', 'w') as NULL:
+        # Start processes
+        processes = []
+        for cmd_str, cmd in zip(cmd_strs, cmds):
+            p_prev = processes[-1][1] if processes else None
 
-    p_last = processes[-1][1]
+            p_stdin = p_prev.stdout if p_prev else None
+            p_stderr = None if show_stderr else NULL
 
-    with open(path, 'w') as f:
-        # Write data to file in chunks (works for arbitrarily large files)
-        while True:
-            data = p_last.stdout.read(512 * 1024)
-            if len(data) == 0:
-                break
-            f.write(data)
+            p_curr = Popen(cmd, env=env, stdout=PIPE, stdin=p_stdin, stderr=p_stderr)
+            processes.append((cmd_str, p_curr))
 
-        # Close processes
-        for cmd_str, p in processes:
-            p.stdout.close()
-            if p.wait() != 0:
-                raise CalledProcessError(cmd=cmd_str, returncode=p.returncode)
+        p_last = processes[-1][1]
+
+        with open(path, 'w') as f:
+            # Write data to file in chunks (works for arbitrarily large files)
+            while True:
+                data = p_last.stdout.read(512 * 1024)
+                if len(data) == 0:
+                    break
+                f.write(data)
+
+            # Close processes
+            for cmd_str, p in processes:
+                p.stdout.close()
+                if p.wait() != 0:
+                    raise CalledProcessError(cmd=cmd_str, returncode=p.returncode)
