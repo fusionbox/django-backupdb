019552da351f821e4c0aae2451353a414697a3f4
David Sanders <dsanders@fusionbox.com>
2013-06-09 19:26:42 -0600

    Clean up processes module

--- backupdb_utils/processes.py
+++ backupdb_utils/processes.py
@@ -4,37 +4,36 @@ import os
 from .streams import err
 
 
+def extend_env(extra_env):
+    env = os.environ.copy()
+    env.update(extra_env)
+    return env
+
+
+def get_env_str(env):
+    return ' '.join("{0}='{1}'".format(k, v) for k, v in env.items())
+
+
 def pipe_commands(cmds, extra_env=None, show_stderr=False, show_last_stdout=False):
     """
     Executes the list of commands piping each one into the next.
     """
-    if extra_env:
-        env = os.environ.copy()
-        env.update(extra_env)
-        env_str = ' '.join("{0}='{1}'".format(k, v) for k, v in extra_env.items()) + ' '
-    else:
-        env = None
-        env_str = ''
-
-    cmd_strs = [
-        '{env_str}{cmd_str}'.format(env_str=env_str, cmd_str=' '.join(cmd))
-        for cmd in cmds
-    ]
+    env = extend_env(extra_env) if extra_env else None
+    env_str = (get_env_str(extra_env) + ' ') if extra_env else ''
+    cmd_strs = [env_str + ' '.join(cmd) for cmd in cmds]
+    num_cmds = len(cmds)
 
     err('* Running `{0}`'.format(' | '.join(cmd_strs)), verbosity=2)
 
     with open('/dev/null', 'w') as NULL:
         # Start processes
-        num_cmds = len(cmds)
         processes = []
         for i, (cmd_str, cmd) in enumerate(zip(cmd_strs, cmds), 1):
-            p_prev = processes[-1][1] if processes else None
-
             if i == num_cmds:
                 p_stdout = None if show_last_stdout else NULL
             else:
                 p_stdout = PIPE
-            p_stdin = p_prev.stdout if p_prev else None
+            p_stdin = processes[-1][1].stdout if processes else None
             p_stderr = None if show_stderr else NULL
 
             p_curr = Popen(cmd, env=env, stdout=p_stdout, stdin=p_stdin, stderr=p_stderr)
@@ -53,18 +52,9 @@ def pipe_commands_to_file(cmds, path, extra_env=None, show_stderr=False):
     Executes the list of commands piping each one into the next and writing
     stdout of the last process into a file at the given path.
     """
-    if extra_env:
-        env = os.environ.copy()
-        env.update(extra_env)
-        env_str = ' '.join("{0}='{1}'".format(k, v) for k, v in extra_env.items()) + ' '
-    else:
-        env = None
-        env_str = ''
-
-    cmd_strs = [
-        '{env_str}{cmd_str}'.format(env_str=env_str, cmd_str=' '.join(cmd))
-        for cmd in cmds
-    ]
+    env = extend_env(extra_env) if extra_env else None
+    env_str = (get_env_str(extra_env) + ' ') if extra_env else ''
+    cmd_strs = [env_str + ' '.join(cmd) for cmd in cmds]
 
     err('* Saving output of `{0}`'.format(' | '.join(cmd_strs)), verbosity=2)
 
@@ -72,9 +62,7 @@ def pipe_commands_to_file(cmds, path, extra_env=None, show_stderr=False):
         # Start processes
         processes = []
         for cmd_str, cmd in zip(cmd_strs, cmds):
-            p_prev = processes[-1][1] if processes else None
-
-            p_stdin = p_prev.stdout if p_prev else None
+            p_stdin = processes[-1][1].stdout if processes else None
             p_stderr = None if show_stderr else NULL
 
             p_curr = Popen(cmd, env=env, stdout=PIPE, stdin=p_stdin, stderr=p_stderr)
