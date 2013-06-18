a1dc047eda97ac119b9e9f644811228b506c3a9b
David Sanders <dsanders@fusionbox.com>
2013-06-17 22:29:28 -0600

    Add tests database settings

--- backupdb/tests/settings.py
+++ backupdb/tests/settings.py
@@ -14,13 +14,20 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite.db',                     # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'backupdb_database',
+        'TEST_NAME': 'test_backupdb_database',
+    },
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'backupdb',
+        'USER': 'root',
+    },
+    'postgresql': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'backupdb',
+        'USER': 'backupdb_user',
+    },
 }
 
 CACHES = {
