b3280a51f78e388bf31a05f27da6a9a5d67f3cf7
David Sanders <dsanders@fusionbox.com>
2013-06-17 21:42:50 -0600

    Add more testing resources

---
+++ backupdb/tests/models.py
@@ -0,0 +1,2 @@
+# flake8: noqa
+from django.db import models
---
+++ backupdb/tests/tests/test_test.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+
+class TestTestCase(TestCase):
+    def test_testing_works(self):
+        self.assertTrue(True)
