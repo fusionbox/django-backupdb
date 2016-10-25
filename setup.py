d6fd6af76b762d10e45f81c64d881e2ebf05e8dc
Julian Andrews <jandrews@fusionbox.com>
2016-10-25 15:45:50 -0600

    Use zest releaser compatible version scheme

--- setup.py
+++ setup.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python
-import subprocess
 import sys
 import os
 
 from setuptools import setup
 
-
-version = (0, 6, 0, 'alpha')
+version = '0.6.0'
 
 current_path = os.path.dirname(__file__)
 
 sys.path.append(current_path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_settings')
-
-
-def get_version():
-    number = '.'.join(map(str, version[:3]))
-    stage = version[3]
-    if stage == 'final':
-        return number
-    elif stage == 'alpha':
-        process = subprocess.Popen('git rev-parse HEAD'.split(), stdout=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        return number + '-' + stdout.decode('utf-8').strip()[:8]
 
 
 def get_readme():
@@ -31,7 +18,7 @@ def get_readme():
 
 setup(
     name='django-backupdb',
-    version=get_version(),
+    version=version,
     description='Management commands for backing up and restoring databases in Django.',
     long_description=get_readme(),
     author='Fusionbox programmers',
